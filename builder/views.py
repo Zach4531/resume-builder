0db37f10280d11f84313dad694a0bf050578335f
Zachary Ciatteo <zciatteo96@gmail.com>
2026-07-20 23:05:02 -0400

    add items to profile page

--- builder/views.py
+++ builder/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Education, Profile, Resume
+from .models import Education, Profile, Resume, Skill, WorkExperience, Certification
 from .forms import ResumeForm
 
 # Create your views here.
@@ -22,4 +22,8 @@ def profile(request, profile_id):
     profile = Profile.objects.get(id=profile_id)
     return render(request, 'builder/profile.html', {
         'profile': profile,
+        'skills': Skill.objects.all(),
+        'jobs': WorkExperience.objects.all(),
+        'certifications': Certification.objects.all(),
+
     })
